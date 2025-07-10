030bf43b6d73d08427a136fcbf0affe63a0d74d7
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-10 08:03:35 +0300

    Adding and fixing content

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  // Filter items based on the selectedCategory
+  const itemsToDisplay = items.filter((item) => {
+    return selectedCategory === "All" || item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">All</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {itemsToDisplay.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            category={item.category}
+          />
         ))}
       </ul>
     </div>
@@ -22,3 +37,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
